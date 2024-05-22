45382c1a787e1fc6f41a34589c83382cc0a2fde0
Alicja <alicjanamieta@gmail.com>
2024-05-22 19:57:10 +0200

    signals in heredoc fixed

--- executor/execute.c
+++ executor/execute.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 15:43:52 by anamieta          #+#    #+#             */
-/*   Updated: 2024/05/22 18:51:13 by mfaoussi         ###   ########.fr       */
+/*   Updated: 2024/05/22 19:44:46 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	execute_child(t_shell *shell, t_node *index, int *fd_pipe)
 	char	*full_path;
 
 	default_child_signals();
+	heredoc(shell);
 	if (index->cmd[0])
 	{
 		full_path = check_cmd_path(index->cmd[0], shell->path);
@@ -70,6 +71,7 @@ void	execute(t_shell *shell)
 {
 	t_node	*index;
 	int		pid;
+	// int		her;
 	int		status;
 	int		tmpin;
 	int		tmpout;
@@ -77,12 +79,11 @@ void	execute(t_shell *shell)
 	tmpout = dup(STDOUT_FILENO);
 	tmpin = dup(STDIN_FILENO);
 	index = shell->s_cmd;
-	print_char(shell->builtins);
-	// if (ft_strncmp("echo", index->cmd[0], 4) == 0)
-	// 	echo(index->cmd);
-	// else
-	// {
-	heredoc(shell);
+	// print_char(shell->builtins);
+	// her = fork();
+	// fork_check(shell, her);
+	// if (her == 0)
+		// heredoc(shell);
 	// print_hd_names(shell);
 	while (index)
 	{
